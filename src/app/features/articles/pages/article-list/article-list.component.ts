e823e9e75ca9ab012b9ccf8dac35292cc35db0f2
Ivan-Andrusyshyn <ivan.andrusyshyn@icloud.com>
2026-09-05 14:29:05 +0300

    add article viewMode for collection

--- src/app/features/articles/pages/article-list/article-list.component.ts
+++ src/app/features/articles/pages/article-list/article-list.component.ts
@@ -1,7 +1,15 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  OnInit,
+  PLATFORM_ID,
+  signal,
+} from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { map } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
 
 // ===============
 import { ArticlesService } from '../../../../core/services/articles.service';
@@ -13,9 +21,10 @@ import { ArticlesService } from '../../../../core/services/articles.service';
   templateUrl: './article-list.component.html',
   styleUrls: ['./article-list.component.scss'],
 })
-export class ArticleListComponent {
+export class ArticleListComponent implements OnInit {
   private readonly route = inject(ActivatedRoute);
   private readonly articlesService = inject(ArticlesService);
+  private readonly platformId = inject(PLATFORM_ID);
 
   private readonly categorySlug = toSignal(
     this.route.paramMap.pipe(map((params) => params.get('categorySlug') ?? '')),
@@ -25,7 +34,18 @@ export class ArticleListComponent {
   readonly category = computed(() =>
     this.articlesService.getCategoryBySlug(this.categorySlug()),
   );
+  ngOnInit(): void {
+    if (!isPlatformBrowser(this.platformId)) {
+      return;
+    }
 
+    setTimeout(() => {
+      document.getElementById('article-list')?.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    });
+  }
   formatDate(dateIso: string): string {
     return new Date(dateIso).toLocaleDateString('uk-UA', {
       day: 'numeric',
--- src/app/shared/components/article-categories/article-categories.component.ts
+++ src/app/shared/components/article-categories/article-categories.component.ts
@@ -1,17 +1,42 @@
-import { Component, inject, input } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import {
+  Component,
+  inject,
+  input,
+  OnInit,
+  PLATFORM_ID,
+  signal,
+} from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 //
-
 import { ArticleCategory } from '../../../features/articles/types/ArticleCategory.type';
+import { isPlatformBrowser } from '@angular/common';
+
+type ViewMode = 'grid' | 'list';
 
 @Component({
   selector: 'app-article-categories',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './article-categories.component.html',
   styleUrl: './article-categories.component.scss',
 })
-export class ArticleCategoriesComponent {
-  categories = input<ArticleCategory[]>();
+export class ArticleCategoriesComponent implements OnInit {
+  categories = input.required<ArticleCategory[]>();
+  isSmallCard = input<boolean>();
+
+  private readonly platformId = inject(PLATFORM_ID);
+  readonly viewMode = signal<ViewMode>('grid');
+
+  ngOnInit(): void {
+    if (!isPlatformBrowser(this.platformId)) return;
+    const mode = localStorage.getItem('viewMode') as ViewMode | undefined;
+
+    this.viewMode.set(mode ?? 'grid');
+  }
+
+  setViewMode(mode: ViewMode): void {
+    this.viewMode.set(mode);
+    localStorage.setItem('viewMode', mode);
+  }
 }
